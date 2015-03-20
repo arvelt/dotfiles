87b48b55cf6fe7c1082b455b060f6403c85c202e
Keisuke Ota <arvelt.s@gmail.com>
2015-03-20 15:38:54 +0900

    add .atom/config.cson

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+ln -fs ~/dotfiles/.atom/config.cson ~/.atom/config.cson
 ln -fs ~/dotfiles/.vimrc ~/.vimrc
-ln -fs ~/dotfiles/.emacs.d  ~/
+#ln -fs ~/dotfiles/.emacs.d  ~/
 ln -fs ~/dotfiles/.bash_profile  ~/.bash_profile
+

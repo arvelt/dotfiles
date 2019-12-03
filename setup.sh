9b13e5929b0757d09c4eff95dccd31a467d8df6a
Keisuke Ota <arvelt.s@gmail.com>
2019-12-03 15:54:01 +0900

    update

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 ln -fs ~/dotfiles/.bash_profile  ~/.bash_profile
-ln -fs ~/dotfiles/neovim/init.vim ~/.config/nvim/init.vim
 
 # VSCode mac
 ln -fs ~/dotfiles/code/settings.json  "$HOME/Library/Application Support/Code/User/settings.json"
